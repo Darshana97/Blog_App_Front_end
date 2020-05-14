50b7f710fbb90d55833f761094e51093ce561d27
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-14 11:32:23 +0530

    article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Article } from "../article";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  selector: "app-article",
+  templateUrl: "./article.component.html",
+  styleUrls: ["./article.component.css"],
 })
 export class ArticleComponent implements OnInit {
+  article: Article = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      (this.article = new Article()), (this.article.key = params.key);
+    });
   }
-
 }
