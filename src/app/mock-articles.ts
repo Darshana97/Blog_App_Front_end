52d1bf02047fe3215c0bdfb5ceaf8c5305a6b97d
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-15 09:07:52 +0530

    mock articles

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -4,7 +4,8 @@ export const ARTICLES: Article[] = [
   {
     id: 1,
     title: "My first article",
-    content: "",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus metus at enim placerat, rutrum tempor ipsum hendrerit. In eget erat magna. Morbi lacinia sollicitudin suscipit. Duis ullamcorper diam sed finibus pulvinar. Nulla nec magna dignissim, tristique elit et, commodo dui. Pellentesque vitae ex euismod, maximus massa a, aliquam eros. Suspendisse potenti. Nunc purus lectus, scelerisque quis fringilla nec, facilisis lobortis ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
     description: "This is my first article!. It's great. Please read it. :)",
     key: "my-first-article",
     date: new Date(),
@@ -14,7 +15,8 @@ export const ARTICLES: Article[] = [
   {
     id: 2,
     title: "The second article",
-    content: "",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus metus at enim placerat, rutrum tempor ipsum hendrerit. In eget erat magna. Morbi lacinia sollicitudin suscipit. Duis ullamcorper diam sed finibus pulvinar. Nulla nec magna dignissim, tristique elit et, commodo dui. Pellentesque vitae ex euismod, maximus massa a, aliquam eros. Suspendisse potenti. Nunc purus lectus, scelerisque quis fringilla nec, facilisis lobortis ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
     description: "Also a great article",
     key: "my-second-article",
     date: new Date(),
