13aa3b22618061ff7f499fdca7b4ab1794823187
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-16 10:06:44 +0530

    title add to article  list

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Article } from "../article";
 import { ARTICLES } from "../mock-articles";
 import { ArticleService } from "../article.service";
+import { Title } from '@angular/platform-browser';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: "app-article-list",
@@ -11,7 +13,11 @@ import { ArticleService } from "../article.service";
 export class ArticleListComponent implements OnInit {
   articles: Article[] = [];
 
-  constructor(private articleService: ArticleService) {}
+  constructor(
+    private articleService: ArticleService,
+    private titleService: Title,
+    private sharedService: SharedService
+  ) {}
 
   ngOnInit(): void {
     this.getArticles();
