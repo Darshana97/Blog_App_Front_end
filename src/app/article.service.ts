7e634a1f182dffabfc8711a4dd6fbd79bafbe4f1
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-17 10:26:10 +0530

    remove mock articles

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Article } from "./article";
-import { ARTICLES } from "./mock-articles";
+// import { ARTICLES } from "./mock-articles";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
